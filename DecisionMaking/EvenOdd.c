067e4018eabe72252b22a60626c1a2271a6831c7
abhijal07 <abhijalchalliyil07@gmail.com>
2026-08-05 23:40:17 +0530

    Add Even or Odd program using if-else

---
+++ DecisionMaking/EvenOdd.c
@@ -0,0 +1,26 @@
+/*
+Program: Even or Odd
+Author: Abhijal C.A.
+Description: Checks whether a given number is even or odd using if-else.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+    {
+        printf("%d is an Even number.\n", num);
+    }
+    else
+    {
+        printf("%d is an Odd number.\n", num);
+    }
+
+    return 0;
+}
