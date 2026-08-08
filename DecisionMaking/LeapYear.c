68216068143ed108d5f884df41f616bc52d3f601
abhijal07 <abhijalchalliyil07@gmail.com>
2026-08-08 12:15:30 +0530

    Add leap year and vowel checking programs

---
+++ DecisionMaking/LeapYear.c
@@ -0,0 +1,34 @@
+/*
+Program: Leap Year
+Author: Abhijal C.A.
+Description: Checks whether a given year is a leap year.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if (year % 400 == 0)
+    {
+        printf("%d is a Leap Year.\n", year);
+    }
+    else if (year % 100 == 0)
+    {
+        printf("%d is not a Leap Year.\n", year);
+    }
+    else if (year % 4 == 0)
+    {
+        printf("%d is a Leap Year.\n", year);
+    }
+    else
+    {
+        printf("%d is not a Leap Year.\n", year);
+    }
+
+    return 0;
+}
---
+++ DecisionMaking/VowelOrConsonant.c
@@ -0,0 +1,27 @@
+/*
+Program: Vowel or Consonant
+Author: Abhijal C.A.
+Description: Checks whether an entered character is a vowel or consonant.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter an alphabet: ");
+    scanf(" %c", &ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    {
+        printf("%c is a Vowel.\n", ch);
+    }
+    else
+    {
+        printf("%c is a Consonant.\n", ch);
+    }
+
+    return 0;
+}
