7adb7d03c720414abf38f6a0e413660349bb34d2
abhijal07 <abhijalchalliyil07@gmail.com>
2026-08-18 21:03:33 +0530

    Add Armstrong and perfect number programs

---
+++ Loops/ArmstrongNumber.c
@@ -0,0 +1,35 @@
+/*
+Program: Armstrong Number
+Author: Abhijal C.A.
+Description: Checks whether a number is an Armstrong number.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num, original, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0)
+    {
+        digit = num % 10;
+        sum = sum + digit * digit * digit;
+        num = num / 10;
+    }
+
+    if (sum == original)
+    {
+        printf("%d is an Armstrong number.\n", original);
+    }
+    else
+    {
+        printf("%d is not an Armstrong number.\n", original);
+    }
+
+    return 0;
+}
---
+++ Loops/PerfectNumber.c
@@ -0,0 +1,34 @@
+/*
+Program: Perfect Number
+Author: Abhijal C.A.
+Description: Checks whether a number is a perfect number.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+
+    if (sum == num)
+    {
+        printf("%d is a Perfect number.\n", num);
+    }
+    else
+    {
+        printf("%d is not a Perfect number.\n", num);
+    }
+
+    return 0;
+}
