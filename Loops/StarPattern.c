68a83d327911a1718e2adfd7954155095c7b73ab
abhijal07 <abhijalchalliyil07@gmail.com>
2026-08-21 23:49:15 +0530

    Add nested loop pattern programs

---
+++ Loops/StarPattern.c
@@ -0,0 +1,27 @@
+ /*
+Program: Star Pattern
+Author: Abhijal C.A.
+Description: Prints a right-angled triangle star pattern.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
