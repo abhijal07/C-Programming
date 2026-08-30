2c8b05e2916130c3019f94490a296b8ce69bf987
abhijal07 <abhijalchalliyil07@gmail.com>
2026-08-30 23:22:54 +0530

    Add recursive factorial and Fibonacci programs

---
+++ Functions/FactorialRecursion.c
@@ -0,0 +1,36 @@
+/*
+Program: Factorial Using Recursion
+Author: Abhijal C.A.
+Description: Calculates the factorial of a number using recursion.
+*/
+
+#include <stdio.h>
+
+long long factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+    {
+        printf("Factorial is not defined for negative numbers.\n");
+    }
+    else
+    {
+        printf("Factorial = %lld\n", factorial(num));
+    }
+
+    return 0;
+}
---
+++ Functions/FibonacciRecursion.c
@@ -0,0 +1,41 @@
+/*
+Program: Fibonacci Series Using Recursion
+Author: Abhijal C.A.
+Description: Generates Fibonacci numbers using recursion.
+*/
+
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", fibonacci(i));
+    }
+
+    printf("\n");
+
+    return 0;
+}
